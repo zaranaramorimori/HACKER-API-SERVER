d29b4c39b3d252890602ee2b5b8816257fc984fb
Kwon Sehoon <devkwonsehoon@naver.com>
2023-01-22 00:23:10 +0900

    chore: Module 내 Provider 코드 일부 수정

--- src/modules/auth/signin.module.ts
+++ src/modules/auth/signin.module.ts
@@ -1,6 +1,6 @@
 import { CacheModule } from "@libraries/cache/cache.module";
 import { UserOrmEntity } from "@libraries/entities/domain/user/user-orm.entity";
-import { Module, Provider } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SigninAdapter } from "./adapters/signin.adapter";
@@ -8,20 +8,18 @@ import { SigninController } from "./controllers/signin.controller";
 import { SIGNIN_INBOUND_PORT } from "./domain/ports/in/signin.port";
 import { SigninService } from "./domain/services/signin.service";
 
-const p: Provider[] = [
-  SigninAdapter,
-  {
-    provide: SIGNIN_INBOUND_PORT,
-    useFactory: signinAdatper => {
-      return new SigninService(signinAdatper, signinAdatper);
-    },
-    inject: [SigninAdapter],
-  },
-];
-
 @Module({
   controllers: [SigninController],
   imports: [TypeOrmModule.forFeature([UserOrmEntity]), CacheModule],
-  providers: [...p],
+  providers: [
+    SigninAdapter,
+    {
+      provide: SIGNIN_INBOUND_PORT,
+      useFactory: signinAdatper => {
+        return new SigninService(signinAdatper, signinAdatper);
+      },
+      inject: [SigninAdapter],
+    },
+  ],
 })
 export class SigninModule {}
--- src/modules/auth/signup.module.ts
+++ src/modules/auth/signup.module.ts
@@ -1,26 +1,24 @@
 import { CacheModule } from "@libraries/cache/cache.module";
 import { UserOrmEntity } from "@libraries/entities/domain/user/user-orm.entity";
-import { Module, Provider } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SignupAdapter } from "./adapters/signup.adapter";
 import { SignupController } from "./controllers/signup.controller";
 import { SIGNUP_INBOUND_PORT } from "./domain/ports/in/signup.port";
 import { SignupService } from "./domain/services/signup.service";
 
-const p: Provider[] = [
-  SignupAdapter,
-  {
-    provide: SIGNUP_INBOUND_PORT,
-    useFactory: signupAdatper => {
-      return new SignupService(signupAdatper, signupAdatper, signupAdatper);
-    },
-    inject: [SignupAdapter],
-  },
-];
-
 @Module({
   controllers: [SignupController],
   imports: [TypeOrmModule.forFeature([UserOrmEntity]), CacheModule],
-  providers: [...p],
+  providers: [
+    SignupAdapter,
+    {
+      provide: SIGNUP_INBOUND_PORT,
+      useFactory: signupAdatper => {
+        return new SignupService(signupAdatper, signupAdatper, signupAdatper);
+      },
+      inject: [SignupAdapter],
+    },
+  ],
 })
 export class SignupModule {}
